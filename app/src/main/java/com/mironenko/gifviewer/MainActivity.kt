3a9add15189ba0500115310bda860f204bbeafa6
VladiMIR-y550 <mironenko.vladimir2019@gmail.com>
2022-07-13 17:45:42 +0300

    start project

---
+++ app/src/main/java/com/mironenko/gifviewer/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mironenko.gifviewer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
